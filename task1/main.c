2adcfe3c9b64143b18ddc2b16550adc7288c8bb1
SC-Fmvp <2506747203@qq.com>
2022-10-26 15:20:31 +0800

    Add hello world3

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,33 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	double a;
+	double f;
+	double h;
+	scanf("%lf %lf",&a,&h);
+	double y=a ;
+	double x=0.001;
+	double i;
+	do{
+		f=(1-h)*y;
+		printf("µ±x=%f,y=%f\n",x,f);
+		x+=h;
+		i=y-f;
+		y=f;
+	}while(i>0.001*h);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	return 0;
 }
 
